8be303c14195b4965cb86b676f742e76c9409222
Unknown <antozegov@foi.hr>
2018-10-25 19:33:56 +0200

    Created timer for seconds
    Created timer for clock that displays clock seconds in seven-segment display.

---
+++ Seven-Segment Clock/Main.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Seven_Segment_Clock
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picSegmentF1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentE1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentC1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentB1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentG1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentA1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentD1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentF2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentE2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentC2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentB2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentG2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentA2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentD2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentF1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentE1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentG1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentD1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentF2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentE2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentG2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentA2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentD2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picSegmentF1
+            // 
+            this.picSegmentF1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentF1.Location = new System.Drawing.Point(23, 34);
+            this.picSegmentF1.Name = "picSegmentF1";
+            this.picSegmentF1.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentF1.TabIndex = 0;
+            this.picSegmentF1.TabStop = false;
+            // 
+            // picSegmentE1
+            // 
+            this.picSegmentE1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentE1.Location = new System.Drawing.Point(23, 120);
+            this.picSegmentE1.Name = "picSegmentE1";
+            this.picSegmentE1.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentE1.TabIndex = 1;
+            this.picSegmentE1.TabStop = false;
+            // 
+            // picSegmentC1
+            // 
+            this.picSegmentC1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentC1.Location = new System.Drawing.Point(99, 120);
+            this.picSegmentC1.Name = "picSegmentC1";
+            this.picSegmentC1.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentC1.TabIndex = 2;
+            this.picSegmentC1.TabStop = false;
+            // 
+            // picSegmentB1
+            // 
+            this.picSegmentB1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentB1.Location = new System.Drawing.Point(99, 34);
+            this.picSegmentB1.Name = "picSegmentB1";
+            this.picSegmentB1.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentB1.TabIndex = 3;
+            this.picSegmentB1.TabStop = false;
+            // 
+            // picSegmentG1
+            // 
+            this.picSegmentG1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentG1.Location = new System.Drawing.Point(35, 104);
+            this.picSegmentG1.Name = "picSegmentG1";
+            this.picSegmentG1.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentG1.TabIndex = 4;
+            this.picSegmentG1.TabStop = false;
+            // 
+            // picSegmentA1
+            // 
+            this.picSegmentA1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentA1.Location = new System.Drawing.Point(35, 19);
+            this.picSegmentA1.Name = "picSegmentA1";
+            this.picSegmentA1.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentA1.TabIndex = 4;
+            this.picSegmentA1.TabStop = false;
+            // 
+            // picSegmentD1
+            // 
+            this.picSegmentD1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentD1.Location = new System.Drawing.Point(35, 190);
+            this.picSegmentD1.Name = "picSegmentD1";
+            this.picSegmentD1.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentD1.TabIndex = 4;
+            this.picSegmentD1.TabStop = false;
+            // 
+            // picSegmentF2
+            // 
+            this.picSegmentF2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentF2.Location = new System.Drawing.Point(122, 34);
+            this.picSegmentF2.Name = "picSegmentF2";
+            this.picSegmentF2.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentF2.TabIndex = 0;
+            this.picSegmentF2.TabStop = false;
+            // 
+            // picSegmentE2
+            // 
+            this.picSegmentE2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentE2.Location = new System.Drawing.Point(122, 120);
+            this.picSegmentE2.Name = "picSegmentE2";
+            this.picSegmentE2.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentE2.TabIndex = 1;
+            this.picSegmentE2.TabStop = false;
+            // 
+            // picSegmentC2
+            // 
+            this.picSegmentC2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentC2.Location = new System.Drawing.Point(198, 120);
+            this.picSegmentC2.Name = "picSegmentC2";
+            this.picSegmentC2.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentC2.TabIndex = 2;
+            this.picSegmentC2.TabStop = false;
+            // 
+            // picSegmentB2
+            // 
+            this.picSegmentB2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentB2.Location = new System.Drawing.Point(198, 34);
+            this.picSegmentB2.Name = "picSegmentB2";
+            this.picSegmentB2.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentB2.TabIndex = 3;
+            this.picSegmentB2.TabStop = false;
+            // 
+            // picSegmentG2
+            // 
+            this.picSegmentG2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentG2.Location = new System.Drawing.Point(134, 104);
+            this.picSegmentG2.Name = "picSegmentG2";
+            this.picSegmentG2.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentG2.TabIndex = 4;
+            this.picSegmentG2.TabStop = false;
+            // 
+            // picSegmentA2
+            // 
+            this.picSegmentA2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentA2.Location = new System.Drawing.Point(134, 19);
+            this.picSegmentA2.Name = "picSegmentA2";
+            this.picSegmentA2.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentA2.TabIndex = 4;
+            this.picSegmentA2.TabStop = false;
+            // 
+            // picSegmentD2
+            // 
+            this.picSegmentD2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentD2.Location = new System.Drawing.Point(134, 190);
+            this.picSegmentD2.Name = "picSegmentD2";
+            this.picSegmentD2.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentD2.TabIndex = 4;
+            this.picSegmentD2.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picSegmentD2);
+            this.Controls.Add(this.picSegmentD1);
+            this.Controls.Add(this.picSegmentA2);
+            this.Controls.Add(this.picSegmentA1);
+            this.Controls.Add(this.picSegmentG2);
+            this.Controls.Add(this.picSegmentG1);
+            this.Controls.Add(this.picSegmentB2);
+            this.Controls.Add(this.picSegmentC2);
+            this.Controls.Add(this.picSegmentB1);
+            this.Controls.Add(this.picSegmentE2);
+            this.Controls.Add(this.picSegmentC1);
+            this.Controls.Add(this.picSegmentF2);
+            this.Controls.Add(this.picSegmentE1);
+            this.Controls.Add(this.picSegmentF1);
+            this.Name = "Main";
+            this.Text = "Seven-segment clock";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentF1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentE1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentG1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentD1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentF2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentE2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentG2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentA2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentD2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picSegmentF1;
+        private System.Windows.Forms.PictureBox picSegmentE1;
+        private System.Windows.Forms.PictureBox picSegmentC1;
+        private System.Windows.Forms.PictureBox picSegmentB1;
+        private System.Windows.Forms.PictureBox picSegmentG1;
+        private System.Windows.Forms.PictureBox picSegmentA1;
+        private System.Windows.Forms.PictureBox picSegmentD1;
+        private System.Windows.Forms.PictureBox picSegmentF2;
+        private System.Windows.Forms.PictureBox picSegmentE2;
+        private System.Windows.Forms.PictureBox picSegmentC2;
+        private System.Windows.Forms.PictureBox picSegmentB2;
+        private System.Windows.Forms.PictureBox picSegmentG2;
+        private System.Windows.Forms.PictureBox picSegmentA2;
+        private System.Windows.Forms.PictureBox picSegmentD2;
+    }
+}
+
---
+++ Seven-Segment Clock/Main.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seven_Segment_Clock
+{
+    public partial class Main : Form
+    {
+        Timer clockTimer = new Timer();
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void ResetColor()
+        {
+            //First digit
+            picSegmentA1.BackColor = ActiveControl.BackColor;
+            picSegmentB1.BackColor = ActiveControl.BackColor;
+            picSegmentC1.BackColor = ActiveControl.BackColor;
+            picSegmentD1.BackColor = ActiveControl.BackColor;
+            picSegmentE1.BackColor = ActiveControl.BackColor;
+            picSegmentF1.BackColor = ActiveControl.BackColor;
+            picSegmentG1.BackColor = ActiveControl.BackColor;
+            //Second digit
+            picSegmentA2.BackColor = ActiveControl.BackColor;
+            picSegmentB2.BackColor = ActiveControl.BackColor;
+            picSegmentC2.BackColor = ActiveControl.BackColor;
+            picSegmentD2.BackColor = ActiveControl.BackColor;
+            picSegmentE2.BackColor = ActiveControl.BackColor;
+            picSegmentF2.BackColor = ActiveControl.BackColor;
+            picSegmentG2.BackColor = ActiveControl.BackColor;
+        }
+        private void DrawSecondDigit(char digit)
+        {
+            switch (digit)
+            {
+                case '0':
+                    picSegmentA2.BackColor = Color.Red;
+                    picSegmentB2.BackColor = Color.Red;
+                    picSegmentC2.BackColor = Color.Red;
+                    picSegmentD2.BackColor = Color.Red;
+                    picSegmentE2.BackColor = Color.Red;
+                    picSegmentF2.BackColor = Color.Red;
+                    break;
+                case '1':
+                    picSegmentB2.BackColor = Color.Red;
+                    picSegmentC2.BackColor = Color.Red;
+                    break;
+                case '2':
+                    picSegmentA2.BackColor = Color.Red;
+                    picSegmentB2.BackColor = Color.Red;
+                    picSegmentG2.BackColor = Color.Red;
+                    picSegmentE2.BackColor = Color.Red;
+                    picSegmentD2.BackColor = Color.Red;
+                    break;
+                case '3':
+                    picSegmentA2.BackColor = Color.Red;
+                    picSegmentB2.BackColor = Color.Red;
+                    picSegmentG2.BackColor = Color.Red;
+                    picSegmentC2.BackColor = Color.Red;
+                    picSegmentD2.BackColor = Color.Red;
+                    break;
+                case '4':
+                    picSegmentF2.BackColor = Color.Red;
+                    picSegmentG2.BackColor = Color.Red;
+                    picSegmentB2.BackColor = Color.Red;
+                    picSegmentC2.BackColor = Color.Red;
+                    break;
+                case '5':
+                    picSegmentA2.BackColor = Color.Red;
+                    picSegmentF2.BackColor = Color.Red;
+                    picSegmentG2.BackColor = Color.Red;
+                    picSegmentC2.BackColor = Color.Red;
+                    picSegmentD2.BackColor = Color.Red;
+                    break;
+                case '6':
+                    picSegmentA2.BackColor = Color.Red;
+                    picSegmentF2.BackColor = Color.Red;
+                    picSegmentE2.BackColor = Color.Red;
+                    picSegmentD2.BackColor = Color.Red;
+                    picSegmentC2.BackColor = Color.Red;
+                    picSegmentG2.BackColor = Color.Red;
+                    break;
+                case '7':
+                    picSegmentA2.BackColor = Color.Red;
+                    picSegmentB2.BackColor = Color.Red;
+                    picSegmentC2.BackColor = Color.Red;
+                    break;
+                case '8':
+                    picSegmentA2.BackColor = Color.Red;
+                    picSegmentB2.BackColor = Color.Red;
+                    picSegmentC2.BackColor = Color.Red;
+                    picSegmentD2.BackColor = Color.Red;
+                    picSegmentE2.BackColor = Color.Red;
+                    picSegmentF2.BackColor = Color.Red;
+                    picSegmentG2.BackColor = Color.Red;
+                    break;
+                case '9':
+                    picSegmentA2.BackColor = Color.Red;
+                    picSegmentB2.BackColor = Color.Red;
+                    picSegmentC2.BackColor = Color.Red;
+                    picSegmentD2.BackColor = Color.Red;
+                    picSegmentF2.BackColor = Color.Red;
+                    picSegmentG2.BackColor = Color.Red;
+                    break;
+            }
+        }
+        private void DrawFirstDigit(char digit)
+        {;
+            switch (digit)
+            {
+                case '0':
+                    picSegmentA1.BackColor = Color.Red;
+                    picSegmentB1.BackColor = Color.Red;
+                    picSegmentC1.BackColor = Color.Red;
+                    picSegmentD1.BackColor = Color.Red;
+                    picSegmentE1.BackColor = Color.Red;
+                    picSegmentF1.BackColor = Color.Red;
+                    break;
+                case '1':
+                    picSegmentB1.BackColor = Color.Red;
+                    picSegmentC1.BackColor = Color.Red;
+                    break;
+                case '2':
+                    picSegmentA1.BackColor = Color.Red;
+                    picSegmentB1.BackColor = Color.Red;
+                    picSegmentG1.BackColor = Color.Red;
+                    picSegmentE1.BackColor = Color.Red;
+                    picSegmentD1.BackColor = Color.Red;
+                    break;
+                case '3':
+                    picSegmentA1.BackColor = Color.Red;
+                    picSegmentB1.BackColor = Color.Red;
+                    picSegmentG1.BackColor = Color.Red;
+                    picSegmentC1.BackColor = Color.Red;
+                    picSegmentD1.BackColor = Color.Red;
+                    break;
+                case '4':
+                    picSegmentF1.BackColor = Color.Red;
+                    picSegmentG1.BackColor = Color.Red;
+                    picSegmentB1.BackColor = Color.Red;
+                    picSegmentC1.BackColor = Color.Red;
+                    break;
+                case '5':
+                    picSegmentA1.BackColor = Color.Red;
+                    picSegmentF1.BackColor = Color.Red;
+                    picSegmentG1.BackColor = Color.Red;
+                    picSegmentC1.BackColor = Color.Red;
+                    picSegmentD1.BackColor = Color.Red;
+                    break;
+                case '6':
+                    picSegmentA1.BackColor = Color.Red;
+                    picSegmentF1.BackColor = Color.Red;
+                    picSegmentE1.BackColor = Color.Red;
+                    picSegmentD1.BackColor = Color.Red;
+                    picSegmentC1.BackColor = Color.Red;
+                    picSegmentG1.BackColor = Color.Red;
+                    break;
+                case '7':
+                    picSegmentA1.BackColor = Color.Red;
+                    picSegmentB1.BackColor = Color.Red;
+                    picSegmentC1.BackColor = Color.Red;
+                    break;
+                case '8':
+                    picSegmentA1.BackColor = Color.Red;
+                    picSegmentB1.BackColor = Color.Red;
+                    picSegmentC1.BackColor = Color.Red;
+                    picSegmentD1.BackColor = Color.Red;
+                    picSegmentE1.BackColor = Color.Red;
+                    picSegmentF1.BackColor = Color.Red;
+                    picSegmentG1.BackColor = Color.Red;
+                    break;
+                case '9':
+                    picSegmentA1.BackColor = Color.Red;
+                    picSegmentB1.BackColor = Color.Red;
+                    picSegmentC1.BackColor = Color.Red;
+                    picSegmentD1.BackColor = Color.Red;
+                    picSegmentF1.BackColor = Color.Red;
+                    picSegmentG1.BackColor = Color.Red;
+                    break;
+            }
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            clockTimer.Interval = 1000;
+            clockTimer.Tick += new EventHandler(this.clockTimer_Tick);
+            clockTimer.Start();
+        }
+
+        private void clockTimer_Tick(object sender, EventArgs e)
+        {
+            ResetColor();
+
+            int clockSeconds = DateTime.Now.Second;
+            string clockSecondsString = clockSeconds.ToString();
+            char num0 = '0';
+            char onlyDigit;
+            char firstDigit;
+            char secondDigit;
+
+            if(clockSeconds < 10)
+            {
+                onlyDigit = clockSecondsString[0];
+                DrawFirstDigit(num0);
+                DrawSecondDigit(onlyDigit);
+            }
+            if (clockSeconds > 9)
+            {
+                firstDigit = clockSecondsString[0];
+                secondDigit = clockSecondsString[1];
+                DrawFirstDigit(firstDigit);
+                DrawSecondDigit(secondDigit);
+            }
+        }
+    }
+}
