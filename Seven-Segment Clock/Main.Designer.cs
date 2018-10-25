2f4fe9712a3137ee07d0abe1eecaf11b1862764f
Unknown <antozegov@foi.hr>
2018-10-25 19:43:59 +0200

    Extra commit

--- Seven-Segment Clock/Main.Designer.cs
+++ Seven-Segment Clock/Main.Designer.cs
@@ -188,7 +188,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 221);
             this.Controls.Add(this.picSegmentD2);
             this.Controls.Add(this.picSegmentD1);
             this.Controls.Add(this.picSegmentA2);
