e18fac50c6c6c8bf7d04ae70ec1d1cb0036b2f0d
Unknown <antozegov@foi.hr>
2018-10-25 18:31:02 +0200

    Created project solution, added .gitignore and .gitattrubites
    Created project solution, added .gitignore and .gitattrubites

---
+++ Seven-Segment Clock/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Seven_Segment_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pcSegmentF = new System.Windows.Forms.PictureBox();
+            this.pbSegmentE = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pcSegmentF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSegmentE)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcSegmentF
+            // 
+            this.pcSegmentF.Location = new System.Drawing.Point(23, 34);
+            this.pcSegmentF.Name = "pcSegmentF";
+            this.pcSegmentF.Size = new System.Drawing.Size(15, 69);
+            this.pcSegmentF.TabIndex = 0;
+            this.pcSegmentF.TabStop = false;
+            // 
+            // pbSegmentE
+            // 
+            this.pbSegmentE.Location = new System.Drawing.Point(23, 120);
+            this.pbSegmentE.Name = "pbSegmentE";
+            this.pbSegmentE.Size = new System.Drawing.Size(15, 69);
+            this.pbSegmentE.TabIndex = 1;
+            this.pbSegmentE.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(98, 120);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(15, 69);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(98, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(15, 69);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pbSegmentE);
+            this.Controls.Add(this.pcSegmentF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pcSegmentF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSegmentE)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcSegmentF;
+        private System.Windows.Forms.PictureBox pbSegmentE;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
