3a1e45d796f92cc85b216094e774d9c8c7937a82
Unknown <antozegov@foi.hr>
2018-10-25 19:43:29 +0200

    Added user control
    Added user control that will be used for displaying digits

---
+++ Seven-Segment Clock/DigitDisplay.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Seven_Segment_Clock
+{
+    partial class DigitDisplay
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picSegmentD2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentD1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentA2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentA1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentG2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentG1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentB2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentC2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentB1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentE2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentC1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentF2 = new System.Windows.Forms.PictureBox();
+            this.picSegmentE1 = new System.Windows.Forms.PictureBox();
+            this.picSegmentF1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentD2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentD1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentA2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentG2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentG1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentE2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentF2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentE1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentF1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picSegmentD2
+            // 
+            this.picSegmentD2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentD2.Location = new System.Drawing.Point(133, 196);
+            this.picSegmentD2.Name = "picSegmentD2";
+            this.picSegmentD2.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentD2.TabIndex = 13;
+            this.picSegmentD2.TabStop = false;
+            // 
+            // picSegmentD1
+            // 
+            this.picSegmentD1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentD1.Location = new System.Drawing.Point(34, 196);
+            this.picSegmentD1.Name = "picSegmentD1";
+            this.picSegmentD1.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentD1.TabIndex = 14;
+            this.picSegmentD1.TabStop = false;
+            // 
+            // picSegmentA2
+            // 
+            this.picSegmentA2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentA2.Location = new System.Drawing.Point(133, 25);
+            this.picSegmentA2.Name = "picSegmentA2";
+            this.picSegmentA2.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentA2.TabIndex = 15;
+            this.picSegmentA2.TabStop = false;
+            // 
+            // picSegmentA1
+            // 
+            this.picSegmentA1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentA1.Location = new System.Drawing.Point(34, 25);
+            this.picSegmentA1.Name = "picSegmentA1";
+            this.picSegmentA1.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentA1.TabIndex = 16;
+            this.picSegmentA1.TabStop = false;
+            // 
+            // picSegmentG2
+            // 
+            this.picSegmentG2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentG2.Location = new System.Drawing.Point(133, 110);
+            this.picSegmentG2.Name = "picSegmentG2";
+            this.picSegmentG2.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentG2.TabIndex = 17;
+            this.picSegmentG2.TabStop = false;
+            // 
+            // picSegmentG1
+            // 
+            this.picSegmentG1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentG1.Location = new System.Drawing.Point(34, 110);
+            this.picSegmentG1.Name = "picSegmentG1";
+            this.picSegmentG1.Size = new System.Drawing.Size(69, 15);
+            this.picSegmentG1.TabIndex = 18;
+            this.picSegmentG1.TabStop = false;
+            // 
+            // picSegmentB2
+            // 
+            this.picSegmentB2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentB2.Location = new System.Drawing.Point(197, 40);
+            this.picSegmentB2.Name = "picSegmentB2";
+            this.picSegmentB2.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentB2.TabIndex = 11;
+            this.picSegmentB2.TabStop = false;
+            // 
+            // picSegmentC2
+            // 
+            this.picSegmentC2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentC2.Location = new System.Drawing.Point(197, 126);
+            this.picSegmentC2.Name = "picSegmentC2";
+            this.picSegmentC2.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentC2.TabIndex = 9;
+            this.picSegmentC2.TabStop = false;
+            // 
+            // picSegmentB1
+            // 
+            this.picSegmentB1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentB1.Location = new System.Drawing.Point(98, 40);
+            this.picSegmentB1.Name = "picSegmentB1";
+            this.picSegmentB1.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentB1.TabIndex = 12;
+            this.picSegmentB1.TabStop = false;
+            // 
+            // picSegmentE2
+            // 
+            this.picSegmentE2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentE2.Location = new System.Drawing.Point(121, 126);
+            this.picSegmentE2.Name = "picSegmentE2";
+            this.picSegmentE2.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentE2.TabIndex = 7;
+            this.picSegmentE2.TabStop = false;
+            // 
+            // picSegmentC1
+            // 
+            this.picSegmentC1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentC1.Location = new System.Drawing.Point(98, 126);
+            this.picSegmentC1.Name = "picSegmentC1";
+            this.picSegmentC1.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentC1.TabIndex = 10;
+            this.picSegmentC1.TabStop = false;
+            // 
+            // picSegmentF2
+            // 
+            this.picSegmentF2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentF2.Location = new System.Drawing.Point(121, 40);
+            this.picSegmentF2.Name = "picSegmentF2";
+            this.picSegmentF2.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentF2.TabIndex = 5;
+            this.picSegmentF2.TabStop = false;
+            // 
+            // picSegmentE1
+            // 
+            this.picSegmentE1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentE1.Location = new System.Drawing.Point(22, 126);
+            this.picSegmentE1.Name = "picSegmentE1";
+            this.picSegmentE1.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentE1.TabIndex = 8;
+            this.picSegmentE1.TabStop = false;
+            // 
+            // picSegmentF1
+            // 
+            this.picSegmentF1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picSegmentF1.Location = new System.Drawing.Point(22, 40);
+            this.picSegmentF1.Name = "picSegmentF1";
+            this.picSegmentF1.Size = new System.Drawing.Size(15, 69);
+            this.picSegmentF1.TabIndex = 6;
+            this.picSegmentF1.TabStop = false;
+            // 
+            // DigitDisplay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.picSegmentD2);
+            this.Controls.Add(this.picSegmentD1);
+            this.Controls.Add(this.picSegmentA2);
+            this.Controls.Add(this.picSegmentA1);
+            this.Controls.Add(this.picSegmentG2);
+            this.Controls.Add(this.picSegmentG1);
+            this.Controls.Add(this.picSegmentB2);
+            this.Controls.Add(this.picSegmentC2);
+            this.Controls.Add(this.picSegmentB1);
+            this.Controls.Add(this.picSegmentE2);
+            this.Controls.Add(this.picSegmentC1);
+            this.Controls.Add(this.picSegmentF2);
+            this.Controls.Add(this.picSegmentE1);
+            this.Controls.Add(this.picSegmentF1);
+            this.Name = "DigitDisplay";
+            this.Size = new System.Drawing.Size(235, 236);
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentD2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentD1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentA2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentG2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentG1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentE2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentF2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentE1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picSegmentF1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picSegmentD2;
+        private System.Windows.Forms.PictureBox picSegmentD1;
+        private System.Windows.Forms.PictureBox picSegmentA2;
+        private System.Windows.Forms.PictureBox picSegmentA1;
+        private System.Windows.Forms.PictureBox picSegmentG2;
+        private System.Windows.Forms.PictureBox picSegmentG1;
+        private System.Windows.Forms.PictureBox picSegmentB2;
+        private System.Windows.Forms.PictureBox picSegmentC2;
+        private System.Windows.Forms.PictureBox picSegmentB1;
+        private System.Windows.Forms.PictureBox picSegmentE2;
+        private System.Windows.Forms.PictureBox picSegmentC1;
+        private System.Windows.Forms.PictureBox picSegmentF2;
+        private System.Windows.Forms.PictureBox picSegmentE1;
+        private System.Windows.Forms.PictureBox picSegmentF1;
+    }
+}
